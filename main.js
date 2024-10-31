27a23a8ce7c08474c9995ab07d8e93949acfae78
chris <christopher.oanea@stu.ocsb.ca>
2024-10-31 11:11:12 -0400

    land landon.html

--- main.js
+++ main.js
@@ -27,3 +27,99 @@ function round(value, decimals) {
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function age(){
+    let x = Number(prompt("How old are you"))
+    if (x >= 66){
+        alert("You get the senior discount")
+        return x
+    }
+    if (x < 16){
+        alert("You can't drive yet")
+        return x
+    }
+    if (x == 44){
+        alert("So is mr squirrel!")
+        return x
+    }
+}
+
+function user_name(){
+    let name = prompt("What is your name?")
+    if (name == "Mr.Squirrel"){
+        alert("So you are Mr.Squirrel!")
+        return name
+    }
+    if (name > 7){
+        alert("You have a long name")
+        return name
+    }
+}
+
+function startgame(){
+    let name = prompt("What is your name?")
+    alert(`Welcome to Saint Matthew's High school ${name}`)
+    let choice = prompt("Go to the (1)basement or go (2)upstairs?")
+    if (choice == 1){
+        basement()
+        return
+    }
+    if (choice == 2){
+        upstairs()
+        return
+    }
+}
+
+function basement(){
+    let choice_1 = prompt("You see a ghost in the distance, do you (1)run up to it or (2)run back upstairs?")
+    if (choice_1 == 1){
+        alert("it was mr flynn and he shot you with a watergun..")
+    }
+    if (choice_1 == 2){
+        alert("Mr flynn started chasing you up the stairs and shot you with a watergun and you slipped and fell")
+    }
+}
+
+function upstairs(){
+    let choice_1 = prompt("Mr brash greets you, do you say (1)Hi or (2)walk right past him?")
+    if (choice_1 == 1){
+        alert("Mr brash said Hi back")
+    }
+    if (choice_1 == 2){
+        alert("Mr brash turns into a ghost and haunts you forever")
+        let brash = prompt("you run away and have a choice to go in mr flynn's class or mr ruddys. do you cleanse yourself with (1)mr flynn's water gun or take a shower in (2)mr ruddy's shower cleaner thing?")
+        if (brash == 1){
+            brash()
+            return
+        }
+        if (brash == 2){
+            ruddy()
+            return
+        }
+    }
+}
+
+function brash(){
+    let choice = prompt("you cleanse yourself with mr flynns special water gun... then you begin to see mr flynn walk into the class... do you (1)hide or (2)fight him?")
+    if (choice == 1){
+        alert("mr flynn destroys you..")
+        alert("end of game!!")
+        return
+    }
+    if (choice == 2){
+        alert("Mr flynn searches the room and doesn't find you.. you live!!!")
+        return
+    }
+}
+
+function ruddy(){
+    let choice = prompt("you cleanse yourself in mr ruddys shower.. do you (1) walk out of the class or (2)jump out the window??")
+    if (choice == 1){
+        alert("mr ruddy finds you and throws a chemical flask at you")
+        return
+    }
+    if (choice == 2){
+        alert("you jump out the window and fall forever")
+        return
+    }
+}
